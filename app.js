069b41f46dc9b38e006d2d5aae0909291d7b1391
b6db07d1a5 <sitthichai.wa@gmail.com>
2020-10-04 17:51:39 +0700

    Add dump sum

--- app.js
+++ app.js
@@ -7,8 +7,20 @@ app.get('/', (req, res) => {
 })
 
 app.get('/heavy-ping', (req, res) => {
+
+    const { query: { n } } = req;
+
+    let sum
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+
+            sum = (i + j) - (j + i) - 1;
+        }
+    }
+
     setTimeout(() => {
-        res.send('heavy-ping');
+        res.send('heavy-ping' + sum);
     }, 50)
 });
 
